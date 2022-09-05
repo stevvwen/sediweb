6748cbc54acae6aaf5323ded807c876cf0b1b6f6
sedimentaryrockstar <sedimentaryrockstr@gmail.com>
2022-09-04 21:06:16 -0400

    intermediate commit

--- app.py
+++ app.py
@@ -10,6 +10,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/research')
+def research():
+    return render_template('research.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/life')
+def life():
+    return render_template('life.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
